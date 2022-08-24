d78011aaf9c7b2b8aa6efa1ee1a0d030317c9361
xGreatSoulx <simonuzarrc@gmail.com>
2022-08-24 17:32:03 -0300

    teste

--- rest.php
+++ rest.php
@@ -3,9 +3,28 @@
     class Rest{
 
         public function Authorization(){
-            $header = getallheaders();
-
-            return $header;
+            $headers = array();
+        foreach ($_SERVER as $key => $value)
+        {
+            if (substr($key, 0, 5) == 'HTTP_')
+            {
+                $header = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
+                $headers[$header] = $value;
+            }
+        }
+        
+        if (function_exists('getallheaders'))
+        {
+            $allheaders = getallheaders();
+            
+            if ($allheaders)
+            {
+                return $allheaders;
+            }
+            
+            return $headers;
+        }
+        return $headers;
         }
 
     }
