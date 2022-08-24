6867d0b4a8f77c34c619235fee063a5de630d9f3
xGreatSoulx <simonuzarrc@gmail.com>
2022-08-24 13:23:44 -0300

    teste

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php 
-    include 'model/ModelBase.php';
-    include 'service/Transaction.php';
+    // include 'model/ModelBase.php';
+    // include 'service/Transaction.php';
 
-    $fodase = new ModelBase();
-    $teste = new Transaction();
-    $resultado = $teste->Transaction();
+    // $fodase = new ModelBase();
+    // $teste = new Transaction();
+    // $resultado = $teste->Transaction();
 
-    echo $resultado;
+    // echo $resultado;
+    phpinfo();
 ?>
