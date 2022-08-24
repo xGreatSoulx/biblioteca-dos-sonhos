053ce13565b4b46cdbe054e5aea090b186e492f8
xGreatSoulx <simonuzarrc@gmail.com>
2022-08-24 10:40:36 -0300

    Demo

--- index.php
+++ index.php
@@ -2,20 +2,9 @@
     include 'model/ModelBase.php';
     include 'service/Transaction.php';
 
-    use Cowsayphp\Farm;
-    use Cowsayphp\Cow;
-
-    $cow = Farm::create(\Cowsayphp\Farm\Cow::class);
-    echo '<pre>'.$cow->say("Ohmg I'm a cow!").'</pre>';
-
     $fodase = new ModelBase();
     $teste = new Transaction();
     $resultado = $teste->Transaction();
 
-    //echo $resultado;
-
-    // $app->get('/cowsay', function() use($app) {
-    //     $app['monolog']->addDebug('cowsay');
-    //     return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
-    // });
+    echo $resultado;
 ?>
